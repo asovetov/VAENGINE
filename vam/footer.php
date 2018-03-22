6a00bb1461e8bed914612fda164422fd515b5be4
asovetov <asovetov@asovetov-Aspire-V5-471>
2018-03-22 11:25:15 +0700

    First commit

---
+++ vam/footer.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+	 * @Project: Virtual Airlines Manager (VAM)
+	 * @Author: Alejandro Garcia
+	 * @Web http://virtualairlinesmanager.net
+	 * Copyright (c) 2013 - 2016 Alejandro Garcia
+	 * VAM is licenced under the following license:
+	 *   Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+	 *   View license.txt in the root, or visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+	 */
+?>
+<br>
+<div class="container-fluid">
+	<hr>
+	<div class="text-center center-block">
+		<p class="txt-railway"><a href="http://sovtrans.tk">Copyright by ВА "SovTrans"</a></p>
+		<a href="https://www.facebook.com/anton.sovetov"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
+		<a href="https://twitter.com/asovetov"><i id="social" class="fa fa-twitter-square fa-3x social-tw"></i></a>
+		<a href="https://plus.google.com/"><i id="social" class="fa fa-google-plus-square fa-3x social-gp"></i></a>
+		<a href="mailto:admin@sovtrans.tk"><i id="social" class="fa fa-envelope-square fa-3x social-em"></i></a>
+		
+	</div>
+</div>
+<br/>
